36bd9e52891635ae539d21675946129fb8d93454
reevedyl-cs <reevedyl@gmail.com>
2026-03-06 05:13:44 -0800

    Add skins list for weapons

---
+++ skins.js
@@ -0,0 +1,14 @@
+const SKINS = [
+  "Desert Eagle | Blaze (Factory New)",
+  "Desert Eagle | Blaze (Minimal Wear)",
+  "AWP | Lightning Strike (Factory New)",
+  "AWP | Lightning Strike (Minimal Wear)",
+  "AWP | Lightning Strike (Field-Tested)",
+  "AK-47 | Fire Serpent (Factory New)",
+  "AK-47 | Fire Serpent (Minimal Wear)",
+  "AK-47 | Fire Serpent (Field-Tested)",
+  "AK-47 | Fire Serpent (Well-Worn)",
+  "AK-47 | Fire Serpent (Battle-Scarred)",
+  "M4A1-S | Blue Phosphor (Factory New)",
+  "M4A1-S | Blue Phosphor (Minimal Wear)"
+];
